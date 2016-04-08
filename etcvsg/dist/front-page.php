4ffec01d2f3a7358ca26f0c09942d0e95cb5e2cd
John Galyon <jgalyon@covhlth.com>
2016-04-08 18:49:02 -0400

    Add ETCVSG Theme

---
+++ etcvsg/dist/front-page.php
@@ -0,0 +1,125 @@
+<?php
+/*
+ * Template Name: Front-Page Template
+ * Version    : 1.0.1
+ * Author     : John Galyon
+ * Author URI : http://www.covenanthealth.com
+ * Created    : December 7, 2015
+ * Updated    : December 21, 2015
+ * @package WordPress
+ * @subpackage Covenant_Health
+*/
+get_header(); ?>
+
+      <?php if (has_post_thumbnail($post->ID)): ?>
+      <?php
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full-size' );
+        $image = $image[0];
+      ?>
+      <?php else :
+        $image = get_bloginfo('stylesheet_directory') . '/img/front-page-assets/default-masthead.jpg';
+      ?>
+      <?php endif; ?>
+      <div class="jumbotron masthead" data-parallax="scroll" data-image-src="<?php echo $image; ?>">
+        <div class="container masthead-container">
+          <div class="row masthead-row">
+            <div class="col-xs-12 col-sm-5 col-md-5 col-lg-4 masthead-quick-links">
+              <?php the_field('masthead_quick_links') ?>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-5 hidden-xs pull-right masthead-copy-area">
+              <?php the_field('masthead_ad_text') ?>
+            </div>
+          </div>
+        </div>
+      </div> <!-- /jumbotron -->
+      <div class="container welcome-container">
+        <div class="row introduction-row">
+          <div class="col-xs-12">
+            <h1><?php the_title(); ?></h1>
+            <main id="post-<?php the_ID(); ?>" <?php post_class($post->post_name); ?>>
+              <?php edit_post_link('<i class="fa fa-pencil"></i> Edit This Page'); ?>
+              <?php the_content(); ?>
+            </main>
+          </div>
+        </div>
+      </div> <!-- /welcome-container -->
+      <div class="container resources-container clearfix">
+        <div class="row resources-row">
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 intro-col services-col">
+            <div class="well">
+              <h3>Services Quick Links</h3>
+              <p>Cardiothoracic surgery is the field of medicine involved in the surgical treatment of organs inside the thorax (chest). This may include treatment of conditions in the heart, lungs and esophagus. Learn more about our services below.</p>
+              <ul class="list--block-display">
+                <li><a href="<?php echo site_url(); ?>/aorticsurgery/">Aortic Surgery</a></li>
+                <li><a href="<?php echo site_url(); ?>/cardiacsurgery/">Cardiac Surgery</a></li>
+                <li><a href="<?php echo site_url(); ?>/esophagealsurgery/">Esophageal Surgery</a></li>
+                <li><a href="<?php echo site_url(); ?>/minimallyinvasivesurgery/">Minimally Invasive Surgery</a></li>
+                <li><a href="<?php echo site_url(); ?>/TAVR/">Transcatheter Aortic Valve Replacement (TAVR)</a></li>
+                <li><a href="<?php echo site_url(); ?>/thoracicsurgery/">Thoracic Surgery</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 intro-col account-col">
+            <div class="well">
+              <h3>Patient Resources</h3>
+              <p>Whether you were referred to us by your primary care physician, cardiologist or pulmonologist, referral to a cardiothoracic surgeon is an emotional experience for you and your family. Learn more below.</p>
+              <ul class="list--block-display">
+                <li><a href="<?php echo site_url(); ?>/insurancequestions/">Accepted Insurances</a></li>
+                <li><a href="<?php echo site_url(); ?>/frequently-asked-questions/">Frequently Asked Questions</a></li>
+                <li><a href="<?php echo site_url(); ?>/locations/">Locations</a></li>
+                <li><a href="<?php echo site_url(); ?>/downloadsandforms/">Patient Forms</a></li>
+              </ul>
+              <h4>Already a patient?</h4>
+              <p>Please let us know how we are doing by and completing a <a href="http://www.surveymonkey.com/r/cmgptsatisfaction">quick survey</a>.</p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 intro-col feed-col">
+            <div class="well">
+              <h3>Facebook Feed</h3>
+              <?php echo do_shortcode('[custom-facebook-feed]') ?>
+            </div>
+          </div>
+        </div> <!-- /resources-row -->
+      </div> <!-- /resources-container -->
+      <div class="jumbotron parallax-window" data-parallax="scroll" data-image-src="<?php echo get_template_directory_uri(); ?>/img/front-page-assets/surgery-background.jpg">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-6">
+              <div class="well translucent-well">
+                <?php the_field('call_to_action') ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> <!-- parallax-window -->
+      <div class="container system-news">
+        <div class="row news-intro-text">
+          <div class="col-xs-12 news-col">
+            <h2><?php echo bloginfo('name'); ?> News</h2>
+          </div>
+        </div> <!-- /news-intro-text -->
+        <div class="row posts-row">
+          <?php
+          // The Query
+          query_posts('posts_per_page=8');?>
+
+          <?php // The Loop
+            while ( have_posts() ) : the_post(); ?>
+          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 post-wrapper">
+            <article class="well">
+              <h2 class="sr-only"><?php the_title_attribute(); ?></h2>
+              <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('post-thumbnail', array( 'class' => "img-responsive post-thumbnail-img")); ?></a>
+              <?php the_excerpt(); ?>
+              <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">Read more</a>
+            </article>
+          </div>
+            <?php endwhile;
+
+            // Reset Query
+            wp_reset_query();
+            ?>
+          </div> <!-- /post-container -->
+        </div> <!-- /news-post-row -->
+      </div> <!-- /system-news -->
+
+<?php get_footer(); ?>
---
+++ etcvsg/gulpfile.js
@@ -0,0 +1,42 @@
+// include gulp
+var gulp = require('gulp');
+
+// plugin manifest
+var jshint  = require('gulp-jshint');
+var sass    = require('gulp-sass');
+var concat  = require('gulp-concat');
+var uglify  = require('gulp-uglify');
+var rename  = require('gulp-rename');
+var bourbon = require('node-bourbon');
+
+// lint task
+gulp.task('lint', function () {
+  return gulp.src('working/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// concatenate and minify js
+gulp.task('scripts', function () {
+  return gulp.src(['working/js/assets/jquery-1.11.3.js', 'working/js/assets/bootstrap.js', 'working/js/assets/parallax.js', 'working/js/assets/smooth-scroll.js', 'working/js/main.js'])
+    .pipe(concat('main.max.js'))
+    .pipe(rename('ch-main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('working/css/*.scss')
+        .pipe(sass({includePaths: require('node-bourbon').includePaths}))
+        .pipe(gulp.dest('dist'));
+});
+
+// watch files for changes
+gulp.task('watch', function () {
+  gulp.watch('working/js/**/*.js', ['lint', 'scripts']);
+  gulp.watch('working/css/**/*.scss', ['sass']);
+});
+
+// default task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
